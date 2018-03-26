67cdc7eb7c4a7c26063b9bd8b6d1787d3def5744
chukwumaokere <chukwumaokere@yahoo.com>
2018-03-26 13:56:16 +0000

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Install clipboardy 
+npm install clipboardy
+# Install sshpass 
+npm install (apt-get install sshpass) or (brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb"
+# Edit 
+config.js
+# Run 
+(preferrably using something like pm2, since the app needs to be constantly monitoring the directory
+# Test. 
+It will output data to console. So use pm2 monit, or just check your command line console for the outputs. 0 means no errors. 1 means an error. You will get at least 2 1's initally, for the temp files created in the directory before the official file
---
+++ gclone.js
@@ -0,0 +1,40 @@
+fs = require("fs");
+var config = require('./config.js');
+var exec = require('child_process').exec;
+const clipboardy = require('clipboardy');
+
+var dir = config.dir;
+var urlbase = config.urlbase;
+var sshuser = config.sshuser;
+var sshpw = config.sshpw;
+var hostname = config.hostname;
+var port = config.port;
+var sspath = config.sspath;
+
+fs.watch(`${dir}`, (eventType, filename) => {
+	//console.log(eventType); //uncomment for debugging
+	//console.log(filename); //uncomment for debugging
+	var filenamebase = filename;
+	filename = filename.replace(/ /g, '\\ ');
+	var path = dir + filename;
+	var url = urlbase + filenamebase;
+	//console.log(path); //uncomment for debugging
+	//TODO: try to find a more secure method for uploading images
+	if (port && port !== NULL && port != ''){
+		var command = `sshpass -p '${sshpw}' scp -r -P ${port} ${path} ${sshuser}@${hostname}:${sspath}`;
+	}else{
+		var command = `sshpass -p '${sshpw}' scp -r ${path} ${sshuser}@${hostname}:${sspath}`;
+	}
+	//console.log(command); //uncomment for debugging
+	var bash = exec(`${command}`, function(err, stdout, stderr){
+		if (err){
+			//do something	
+		//	console.log(stderr);
+		}
+		//console.log(stdout);
+	});
+	bash.on('exit', function(code){
+		console.log(code); //should print out 0 if no errors
+	});
+	clipboardy.writeSync(url); //writes URL to the clipboard for easy pasting. 
+});
