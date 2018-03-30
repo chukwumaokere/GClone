a88c1bfb6ef9bb0e64b2b084ad4d55f237849ac5
chukwumaokere <chukwumaokere@yahoo.com>
2018-03-30 05:52:47 +0000

    changing core to use sftp-upload instead

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
-exports.dir = '/path/to/screenshot/directory/'; //Keep the trailing slash /
-exports.urlbase = 'http://website.com/Screenshots/'; //The web directory you want to upload to
+exports.dir = ''; //The directory to monitor. Keep the trailing slash /
+exports.urlbase = ''; //The web directory you want to upload to
 
 exports.sshuser = ''; //User for ssh
 exports.sshpw = ''; //Password to SSH 
 exports.hostname = ''; //Host to SSH into. Can take domainname.com, subdomain.domainname.com, or IP.
 exports.port = ''; //Port for SSH IF you have a different port configured, otherwise leave blank.
-exports.sspath = '/var/www/html/Screenshots/'; //The path to the 'Screenshots/' directory after SSH. Please make it an absolute path, not relative.
+exports.rsa_key_path = ''//path to your local id_rsa file 
+exports.sspath = ''; //The path to the 'Screenshots/' directory after SSH. Please make it an absolute path, not relative. Keep the trailing slash / 
 
--- gclone.js
+++ gclone.js
@@ -3,6 +3,7 @@ var config = require('./config.js');
 var exec = require('child_process').exec;
 const clipboardy = require('clipboardy');
 
+//Variables from config
 var dir = config.dir;
 var urlbase = config.urlbase;
 var sshuser = config.sshuser;
@@ -10,74 +11,44 @@ var sshpw = config.sshpw;
 var hostname = config.hostname;
 var port = config.port;
 var sspath = config.sspath;
+var rsa_key_path = config.rsa_key_path;
 
-var error = 0;
-var d = new Date();
+//For error processing
+var errmsg;
 
-fs.watch(`${dir}`, (eventType, filename) => {
-	//console.log(eventType); //uncomment for debugging
-	//console.log(filename); //uncomment for debugging
+//sftp protocol
+var SftpUpload = require('sftp-upload'),
+        fs = require('fs');
+ 
+    var options = {
+        host: hostname,
+        username: sshuser,
+	port: port,
+        path: dir,
+        remoteDir: sspath,
+        privateKey: fs.readFileSync(rsa_key_path),
+	passphrase: ''
+    },
+    sftp = new SftpUpload(options);
+
+//Execute on file added to 
+ fs.watch(`${dir}`, (eventType, filename) => { //Need to ignore Thumb.db and only upload as many files as are new.
 	var filenamebase = filename;
-	filename = filename.replace(/ /g, '\\ ');
-	var path = dir + filename;
+    filename = filename.replace(/ /g, '\\ ');
 	var url = urlbase + filenamebase;
-	//console.log(path); //uncomment for debugging
-	//TODO: try to find a more secure method for uploading images
-	if (port && port !== undefined && port != ''){
-		var command = `sshpass -p '${sshpw}' scp -r -P ${port} ${path} ${sshuser}@${hostname}:${sspath}`;
-	}else{
-		var command = `sshpass -p '${sshpw}' scp -r ${path} ${sshuser}@${hostname}:${sspath}`;
-	}
-	//console.log(command); //uncomment for debugging
-
-	//Init variables for error logging
-	var stdop;
-	var stder;
-	var code;
-	var errmsg; 
-
-	var bash = exec(`${command}`, function(err, stdout, stderr){
-		if (err){
-			//do something	
-		//	console.log(stderr); //uncomment for debugging
-			var stder = stderr;
-		
-		}
-		//console.log(stdout); //uncomment for debugging
-		var stdop = stdout;
-	});
-	bash.on('exit', function(code){
-		//console.log(code); //should print out 0 if no errors
-		var code = code;
-	});
-
-	//Added more user friendly error handling
-	if (typeof code !== 'undefined'){
-		if (code == 0){
-			clipboardy.writeSync(url); //writes URL to the clipboard for easy pasting. 
-		}else{
-			console.log('returned error code was not 1');
-			error = 1;
-		}
-	}else {
-		console.log('code is undefined');
-		error = 1;
-	}
-
-	//Check error state
-	if (error > 0){
-		console.log(error);
+	
+    sftp.on('error', function(err) {
 		var errmsg = 'There was an error pushing the file to the remote system, try uncommenting debug lines or view log.txt for more info.';
 		clipboardy.writeSync(errmsg);
-	}
-
-	//Write to log file
-	fs.appendFile('./log.txt', `${d}: ${errmsg} Program threw code ${code}. Tried to run command: ${command}. Output was ${stdop}. Error was ${stder}.`, function(err){
-		if (err){
-			console.log('Could not write to file, check permissions');
-		}
-		console.log('Wrote to log.txt');
-	});
-});
-
-//nothing to see here. keep on moving
+        throw err;
+    })
+    .on('uploading', function(progress) {
+        console.log('Uploading', progress.file);
+        console.log(progress.percent+'% completed');
+    })
+    .on('completed', function() {
+        console.log('Upload Completed');
+		clipboardy.writeSync(url); //writes URL to the clipboard for easy pasting.
+    })
+    .upload();
+ });
